f8892cd963f4298a4a9223d604f61a67050380f4
hydralgorithm <hydralgorithm@gmail.com>
2025-12-04 20:58:31 +0530

    Second commit

--- lessons/Chapt0.py
+++ lessons/Chapt0.py
@@ -1,3 +1,39 @@
 # This is my first python program
 print("I love pizza !")
 print("It's really really good !")
+# LETS DIVE INTO DIFFERENT TYPES OF VARIABLES
+# Strings
+first_name = "Hydrizzle"
+food = "pizza"
+email = "random123@gmail.com"
+print(first_name)
+# Lets use format string
+print(f"Hello I'm {first_name}")
+print(f"You like {food}")
+print(f"Email: {email}")
+#Integers
+age = 18
+quantity = 3
+num_of_students = 67
+print(f"You are {age} years old")
+print(f"You are buying {quantity} pizzas!")
+print(f"Your class has {num_of_students} students.")
+# Float
+price = 10.99
+gpa = 8.2
+distance = 5.5
+print(f"The price is ${price}.")
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance}km")
+# Boolean
+is_student = False
+for_sale = False
+if for_sale:
+    print("Its for sale!")
+else:
+    print("Not for sale!")
+print(f"Are you a student? : {is_student}")
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
