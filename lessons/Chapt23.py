6611baa2917105fecd7285be6eeeea534deb2ee9
hydralgorithm <hydralgorithm@gmail.com>
2025-12-07 03:01:58 +0530

    Modules

---
+++ lessons/Chapt23.py
@@ -0,0 +1,18 @@
+# Module
+
+# print(help("modules"))
+# print(help("math"))
+
+# import math as m
+# print(m.pi)
+
+# from math import pi
+# print(pi)
+
+import exModule
+# result = exModule.pi
+# result = exModule.square(3)
+# result = exModule.cube(3)
+# result = exModule.circumference(3)
+result = exModule.area(3)
+print(result)
