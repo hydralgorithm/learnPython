7f7780ffa5300c1b64549014ca8812e872ae3b5d
hydralgorithm <hydralgorithm@gmail.com>
2025-12-08 22:41:02 +0530

    Duck typing

---
+++ lessons/Chapt31.py
@@ -0,0 +1,24 @@
+# Polymorphism using Duck Typing
+# if it looks like a duck and quaks like a duck, it must be a duck 🗣️🗣️🗣️
+
+class Animal:
+    alive = True
+
+class Dog(Animal):
+    def speak(self):
+        print("WOOF!")
+
+class Cat(Animal):
+    def speak(self):
+        print("MEOW!")
+
+class Car:
+    alive = False
+    def speak(self):
+        print("HONK!")
+
+animals = [Dog(), Cat(), Car()]
+
+for animal in animals:
+    animal.speak()
+    print(animal.alive)
