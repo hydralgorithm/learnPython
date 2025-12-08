b749c6d3c3c6e3e9667c17abfb96993af4ffb398
hydralgorithm <hydralgorithm@gmail.com>
2025-12-08 18:31:39 +0530

    Super function

---
+++ lessons/Chapt29.py
@@ -0,0 +1,32 @@
+# super() function
+
+class Shape:
+    def __init__(self, color, is_filled):
+        self.color = color
+        self.is_filled = is_filled
+
+
+class Circle(Shape):
+    def __init__(self, color, is_filled, radius):
+        super().__init__(color, is_filled)
+        self.radius = radius
+
+class Square(Shape):
+    def __init__(self, color, is_filled, width):
+        super().__init__(color, is_filled)
+        self.width = width
+
+class Triangle(Shape):
+    def __init__(self, color, is_filled, width, height):
+        super().__init__(color, is_filled)
+        self.width = width
+        self.height = height
+
+circle = Circle(color = "Red",is_filled = True,radius = 5)
+square = Square(color = "Brown",is_filled = False,width = 6)
+triangle = Triangle(color = "Green",is_filled = True,width = 4, height = 10)
+
+print(triangle.color)
+print(triangle.is_filled)
+print(triangle.width)
+print(triangle.height)
