a88e346c42e2822c4be13784090584e9c1a763a0
hydralgorithm <hydralgorithm@gmail.com>
2025-12-09 20:51:19 +0530

    DateTime

---
+++ lessons/Chapt41.py
@@ -0,0 +1,26 @@
+# Dates and times
+import datetime
+
+date = datetime.date(2025, 1, 2) #Y-M-D
+today = datetime.date.today()
+
+time = datetime.time(12, 30, 0)
+now = datetime.datetime.now()
+
+# Formatting
+now = now.strftime("%H:%M:%S %d-%m-%Y")
+
+# print(date)
+# print(today)
+
+# print(time)
+# print(now)
+
+# EXERCISE
+target_datetime = datetime.datetime(2020, 1, 2, 12, 30, 1)
+current_datetime = datetime.datetime.now()
+
+if target_datetime < current_datetime:
+    print("Target date has passed")
+else:
+    print("Target date has NOT passed")
